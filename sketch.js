c972ee23d18da252e0b4040e3740d775623a108f
Saloni83 <75675666+Saloni83@users.noreply.github.com>
2020-12-16 15:56:46 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+var movingRect,fixedRect;
+
+
+function setup() {
+  createCanvas(800,400);
+  movingRect = createSprite(100,200 ,20, 50);
+  movingRect.shapeColor="green";
+
+  fixedRect = createSprite(400,200,50,50)
+  fixedRect.shapeColor="green";
+}
+
+function draw() {
+  background(0);  
+movingRect.x=mouseX;
+movingRect.y=mouseY;
+
+if(movingRect.x-fixedRect.x<movingRect.width/2+fixedRect.width/2&&
+  fixedRect.x-movingRect.x<movingRect.width/2+fixedRect.width/2&&
+  movingRect.y-fixedRect.y<movingRect.height/2+fixedRect.height/2&&
+  fixedRect.y-movingRect.y<movingRect.height/2+fixedRect.height/2){
+fixedRect.shapeColor="yellow";
+movingRect.shapeColor="yellow";
+}
+else{
+  fixedRect.shapeColor="green";
+  movingRect.shapeColor="green";
+}
+  drawSprites();
+}
